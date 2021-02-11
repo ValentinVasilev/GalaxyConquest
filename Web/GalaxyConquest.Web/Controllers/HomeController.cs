af5c1334520f17263887c8db7acad12feec90d62
ValentinVasilev <Shirleymay@abv.bg>
2021-02-11 18:41:33 +0200

    Add Cards
    Fix links to cards on Index Page

--- Web/GalaxyConquest.Web/Controllers/HomeController.cs
+++ Web/GalaxyConquest.Web/Controllers/HomeController.cs
@@ -38,6 +38,11 @@
             return this.View();
         }
 
+        public IActionResult SupportUs()
+        {
+            return this.View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
